6eb5c54f5bcb151ead53adaafce3e3d81da37f82
mocota <30808sugimotomoe@gmail.com>
2026-03-22 17:50:08 +0900

    モーダルJS実装

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,61 @@
-const
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    // 詳しく見るボタンを全部取得（querySelectorAllは、指定したセレクタに一致するすべての要素を取得する）
+    const openButtons = document.querySelectorAll(".open-modal");
+
+    // モーダル本体を全部取得
+    const modals = document.querySelectorAll(".modal");
+
+    // 「閉じる」ボタンを全部取得
+    const closeButtons = document.querySelectorAll(".close");
+
+    // 開く処理。それぞれ処理する
+        // forEachとは？　配列の要素を一つずつ処理するためのメソッド
+
+    openButtons.forEach(button => {
+
+        // 各ボタンにクリックイベントをつける
+        button.addEventListener("click", () => {
+// modalIDという名前の定数に、modal1とかを入れる。
+            const modalId = button.dataset.modal;
+
+            // 取得したIDのモーダルを探す
+            const modal = document.getElementById(modalId);
+
+            // もしモーダルが存在するならばactiveクラスをつける
+            if (modal) {
+                modal.classList.add("active");
+            }
+        });
+    });
+
+
+// 閉じる処理。それぞれ処理する
+    closeButtons.forEach(button => {
+// 各ボタンにクリックイベントをつける
+        button.addEventListener("click", () => {
+
+            // closestでbuttonから一番近い親要素のmodalクラスを探す
+            const modal = button.closest(".modal");
+
+            // activeクラスを外す
+            modal.classList.remove("active");
+        });
+    });
+
+// 閉じる以外にも背景クリックで閉じるようにする
+    modals.forEach(modal => {
+// eにクリック情報を入れる
+        modal.addEventListener("click", (e) => {
+
+            // e.targetはクリックされた場所？？？？"
+            // もし背景がクリックされたなら、activeクラスを外す
+            if (e.target === modal) {
+                modal.classList.remove("active");
+            }
+        });
+    });
+
+});
